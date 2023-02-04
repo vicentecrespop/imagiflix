fe93ec229a82e44966dc5dfac731730de8cbc3fb
vicentecrespop <vicentecrespo45@gmail.com>
2023-02-04 13:54:54 -0300

    added genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 import { APIKEY, URL, EVENTS } from './data/constants';
 import emmiter from './utils/eventEmmiter';
